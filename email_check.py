1708c58946c6bb51ce3341f53057621c17d64a4e
Richard Williams <richard.lluttrell@gmail.com>
2019-03-01 13:03:45 -0700

    fixed declined isse

--- email_check.py
+++ email_check.py
@@ -21,23 +21,27 @@ fname = 0
 lname = 1
 email = 2
 changed = 3
+accepted_declines = {'declined','delcined','decline','declind','declin'}
 
 for index,row in email_list.iterrows():
     suggestion = pymailcheck.suggest(row[email])
 
     ## if we want to replace all the emails
     ## with the fixed versions without checking
-    if (suggestion):
+    if row[email].lower() in accepted_declines:
+        continue
+    elif (suggestion):
         fixed_email = suggestion['full']
-        fixed_full = row[fname],row[lname],fixed_email,True
+        fixed_full = row[fname],row[lname],fixed_email,row[email],True
         row[email] = fixed_email
+        fixed_list.append(fixed_full)
     else:
         fixed_full = row[fname],row[lname],row[email],False
-    fixed_list.append(fixed_full)
+        fixed_list.append(fixed_full)
 
 fixed_listdf = pd.DataFrame(fixed_list)
 
-t = fixed_listdf.loc[fixed_listdf[3] == True]
+t = fixed_listdf.loc[fixed_listdf[4] == True]
 f = fixed_listdf.loc[fixed_listdf[3] == False]
-t.to_csv(r'change.csv',header=False, index=False,columns=(0,1,2))
+t.to_csv(r'change.csv',header=False, index=False,columns=(0,1,2,3))
 f.to_csv(r'output.csv',header=False, index=False,columns=(0,1,2))
