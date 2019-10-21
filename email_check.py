86ef51c294865539d5ed77c6e358ab8f0692f414
Donovan Paget <donovan.p56@gmail.com>
2019-10-20 18:37:12 -0600

    fixed declined bug

--- email_check.py
+++ email_check.py
@@ -1,4 +1,3 @@
-import sys
 import pandas as pd
 import pymailcheck as pymailcheck
 from tkinter import Tk
@@ -6,11 +5,11 @@ from tkinter.filedialog import askopenfilename
 
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-print(filename)
-
+print("Opening from: " + filename)
 
 #create pandas dataframe from csv file
 email_list = pd.read_csv(filename, header=None)
+
 fixed_list = list()
 fname = 0
 lname = 1
@@ -23,7 +22,7 @@ for index,row in email_list.iterrows():
 
     ## if we want to replace all the emails
     ## with the fixed versions without checking
-    if row[email].lower() in accepted_declines:
+    if any(ele in row[email].lower() for ele in accepted_declines):
         continue
     elif (suggestion):
         fixed_email = suggestion['full']
@@ -40,3 +39,5 @@ t = fixed_listdf.loc[fixed_listdf[4] == True] ## change
 f = fixed_listdf.loc[fixed_listdf[3] == False] ## output (no change)
 t.to_csv(r'incorrect.csv',header=("First Name","Last Name","Suggested Email","Current Email"), index=False,columns=(0,1,2,3))
 f.to_csv(r'correct.csv',header=("First Name","Last Name","Email"), index=False,columns=(0,1,2))
+print("Correct emails in correct.csv")
+print("Incorrect emails and their suggested fixes in incorrect.csv")
